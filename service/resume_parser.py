c8792c7cdf68fc4b41db8dcdcf27cb9b847db534
Sai4028 <malleli.saiwrk@gmail.com>
2026-06-21 10:44:52 +0530

    Update resume_parser.py

--- service/resume_parser.py
+++ service/resume_parser.py
@@ -1,1 +1,17 @@
+import pdfplumber
 
+def extract_resume_text(uploaded_file):
+    text = ""
+
+    try:
+        with pdfplumber.open(uploaded_file) as pdf:
+            for page in pdf.pages:
+                page_text = page.extract_text()
+
+                if page_text:
+                    text += page_text + "\n"
+
+    except Exception as e:
+        text = f"Error reading PDF: {str(e)}"
+
+    return text

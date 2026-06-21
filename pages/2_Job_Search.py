6fea9f3d522d1f656e7e1962c775c8df4d274931
Sai4028 <malleli.saiwrk@gmail.com>
2026-06-21 11:34:18 +0530

    Update 2_Job_Search.py

--- pages/2_Job_Search.py
+++ pages/2_Job_Search.py
@@ -5,45 +5,4 @@ st.title("Job Search")
 
 profile = load_profile()
 
-if not profile:
-    st.warning("Please create your profile first.")
-    st.stop()
-
-ai_profile = profile.get("ai_profile", {})
-
-st.subheader("Recommended Roles")
-
-recommended_roles = ai_profile.get(
-    "recommended_roles",
-    []
-)
-
-selected_roles = []
-
-for role in recommended_roles:
-
-    if st.checkbox(
-        role,
-        value=True
-    ):
-        selected_roles.append(role)
-
-st.subheader("Locations")
-
-locations = profile.get(
-    "locations",
-    ""
-)
-
-st.write(locations)
-
-st.divider()
-
-st.write("Selected Roles")
-
-st.write(selected_roles)
-
-if st.button("Search Jobs"):
-    st.success(
-        f"Searching jobs for {len(selected_roles)} roles..."
-    )
+st.write(profile)

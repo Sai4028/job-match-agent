8b78fbaa5aca0e32ec43f793c06b668fa6039ca3
Sai4028 <malleli.saiwrk@gmail.com>
2026-06-21 10:58:48 +0530

    Update profile_manager.py

--- service/profile_manager.py
+++ service/profile_manager.py
@@ -1,1 +1,19 @@
+import json
+import os
 
+PROFILE_PATH = "data/profile.json"
+
+def save_profile(profile_data):
+
+    os.makedirs("data", exist_ok=True)
+
+    with open(PROFILE_PATH, "w") as f:
+        json.dump(profile_data, f, indent=4)
+
+def load_profile():
+
+    if os.path.exists(PROFILE_PATH):
+        with open(PROFILE_PATH, "r") as f:
+            return json.load(f)
+
+    return None

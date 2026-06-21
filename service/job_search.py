b39a7c6b6fec613c30449545c951fd2e50246922
Sai4028 <malleli.saiwrk@gmail.com>
2026-06-21 11:43:55 +0530

    Add job search function with sample job listings

--- service/job_search.py
+++ service/job_search.py
@@ -1,1 +1,26 @@
 
+def search_jobs(selected_roles):
+
+    jobs = [
+
+        {
+            "title": "Senior Product Manager",
+            "company": "Microsoft",
+            "location": "Remote"
+        },
+
+        {
+            "title": "AI Product Manager",
+            "company": "Google",
+            "location": "Bangalore"
+        },
+
+        {
+            "title": "Platform Product Manager",
+            "company": "Atlassian",
+            "location": "Remote"
+        }
+
+    ]
+
+    return jobs
